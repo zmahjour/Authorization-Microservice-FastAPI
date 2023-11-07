5ffaa465d35f1a4735f844ad070a948c13decf94
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 17:03:32 +0330

    chore: define user_register api

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -7,3 +7,32 @@ from schemas.users import UserRegister
 
 
 router = APIRouter(prefix="/users", tags=["users"])
+
+
+@router.post("/register")
+async def user_register(user: UserRegister):
+    user_data = jsonable_encoder(user)
+    account_register_url = "http://localhost:8001/account/register"
+
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url=account_register_url, json=user_data)
+
+        if response.status_code == status.HTTP_406_NOT_ACCEPTABLE:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                detail=json.loads(response.text).get("detail"),
+            )
+        elif response.status_code == status.HTTP_409_CONFLICT:
+            raise HTTPException(
+                status_code=status.HTTP_406_NOT_ACCEPTABLE,
+                detail=json.loads(response.text).get("detail"),
+            )
+        elif response.status_code == status.HTTP_201_CREATED:
+            return JSONResponse(
+                content=json.loads(response.text), status_code=status.HTTP_201_CREATED
+            )
+        else:
+            return HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=json.loads(response.text).get("detail"),
+            )
