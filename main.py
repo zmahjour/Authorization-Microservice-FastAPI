c6b0673ee0e7044572f6c103402900df5d65a3b2
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 22:59:10 +0330

    chore: define redis_lifespan to handle redis connection

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import aioredis
 from api.api_v1.api import router as auth_router
 from core.config import settings
 
+
+@asynccontextmanager
+async def redis_lifespan(app: FastAPI):
+    app.state.redis = await aioredis.from_url(
+        f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/{settings.REDIS_DB}"
+    )
+    yield
+    await app.state.redis.close()
+
+
+app = FastAPI(lifespan=redis_lifespan)
 app.include_router(auth_router)
 
 
