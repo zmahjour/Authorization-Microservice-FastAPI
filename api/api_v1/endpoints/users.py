0f4b3460089298cc8035ef44d59e1f0f382277d8
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 23:02:18 +0330

    chore: import Request, UserLogin, JWTToken and settings

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -1,9 +1,11 @@
-from fastapi import APIRouter, status, HTTPException
+from fastapi import APIRouter, status, HTTPException, Request
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import httpx
 import json
-from schemas.users import UserRegister
+from schemas.users import UserRegister, UserLogin
+from auth.jwt import JWTToken
+from core.config import settings
 
 
 router = APIRouter(prefix="/users", tags=["users"])
