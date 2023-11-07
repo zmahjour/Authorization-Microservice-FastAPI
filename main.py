13f799571987615d197f8a9798ecaf6bd482f574
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 16:57:40 +0330

    fix: edit config to main

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app.include_router(auth_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("config:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8002, reload=True)
