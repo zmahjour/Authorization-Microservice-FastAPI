f260d835e480b4d51d6a8662196a3ebea57472e9
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-07 16:58:57 +0330

    chore: define UserBase and UserRegister schemas

--- schemas/users.py
+++ schemas/users.py
@@ -1,1 +1,13 @@
 from pydantic import BaseModel, EmailStr
+
+
+class UserBase(BaseModel):
+    username: str
+    email: EmailStr
+
+
+class UserRegister(UserBase):
+    password: str
+    confirm_password: str
+
+
