dc700cd8696e371d73558020af7c4909f57c539c
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-08 18:56:18 +0330

    chore: define get_token_from_header method

--- auth/jwt_auth.py
+++ auth/jwt_auth.py
@@ -4,3 +4,8 @@ from core.config import settings
 
 
 class JWTAuthentication:
+    @staticmethod
+    def get_token_from_header(header):
+        token = header.replace("Bearer", "").replace(" ", "")
+        return token
+
