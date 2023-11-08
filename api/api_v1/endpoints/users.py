06484fa06f5e7de9ed0871e2340639af7d927278
Zahra Mahjour <zahra.mahjour@gmail.com>
2023-11-08 13:54:24 +0330

    chore: import Header

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, status, HTTPException, Request
+from fastapi import APIRouter, status, HTTPException, Request, Header
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import httpx
